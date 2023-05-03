9d1b7fd56ee805d07ca1b46a4d637741d04cf66d
Robert Jensen <robert@robert-jensen.dk>
2023-05-03 21:41:31 +0200

    save

--- api/main.go
+++ api/main.go
@@ -4,11 +4,10 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/rhjensen79/files2env"
 )
 
 func main() {
-	files2env.Import("/bindings/tanzuquiz-db")
+	//files2env.Import("/bindings/tanzuquiz-db")
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
